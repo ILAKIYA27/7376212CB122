ec06c8f38df1c49eb0db481fbd9023188c46fd9b
ILAKIYA\Kalai Kumar R <kalaikumar.cs18@bitsathy.ac.in>
2024-03-17 16:26:03 +0530

    commiting the code for http server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Ilakiya")
+	})
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "7376212CB122")
+	})
+	http.HandleFunc("/department", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "CSBS")
+	})
+	http.HandleFunc("/color", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Black")
+	})
+
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
